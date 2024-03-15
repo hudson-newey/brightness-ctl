671d47fa745cc5316ed3f68b5cc6605914d415e1
hudson-newey <grath@duck.com>
2024-03-16 00:34:35 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # brightness-ctl
-A simple program that can adjust brightness in x11 while maintaining the correct color balence and contrast
+
+A simple program that can adjust brightness in x11 while maintaining the correct color balance and contrast
+
+This is a work in progress (I'm still figuring out what mathematical function of brightness I want gamma to follow), but I know I don't like the one that X11 comes with.
---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::env;
+use std::f64::consts::E;
+use std::process::Command;
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+
+    if args.len() <= 1 {
+        println!("Error: No brightness value provided");
+        println!("Usage: brightness <brightness>");
+        return;
+    }
+
+    let brightness: f64 = args[1].parse::<f64>().unwrap();
+    let gamma: f64 = calculate_gamma(brightness);
+
+    change_output(brightness, gamma);
+}
+
+fn calculate_gamma(brightness: f64) -> f64 {
+    // I don't know if "turning point" is the correct term
+    // what I'm trying to say is that in exponential function
+    // when x = 0.85, y = 0.85
+    const TURNING_POINT: f64 = 0.8;
+    let m = TURNING_POINT / E.powf(TURNING_POINT);
+    let c = 1.0 - (m * E.powf(1.0));
+
+    let gamma = m * E.powf(brightness) + c;
+
+    println!("Gamma: {}", gamma);
+
+    return gamma;
+}
+
+fn change_output(brightness: f64, gamma: f64) {
+    let command: String = format!(
+        "xrandr --output eDP-1 --brightness {} --gamma {}",
+        brightness, gamma
+    );
+
+    Command::new("sh")
+        .arg("-c")
+        .arg(command)
+        .output()
+        .expect("failed to execute process");
+}
